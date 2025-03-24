0ee0a0889c8d70629cd93c1b65ab164d3ff96095
Mark MacGowan <130069175+MarkMacGowan@users.noreply.github.com>
2025-03-24 19:31:50 +0000

    Another fix hopfully

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ sudo npm install -g pm2
 # stop any instance of our application running currently 
 pm2 stop example_app
 # change directory into folder where application is downloaded
-cd ExampleApplication/ 
+cd ExampleApp/ 
 # Install application dependencies
 npm install
 # start the application with the process name ExampleApp using pm2
